b1e0a7f73c78b481b3737284d7e6cb5674d1ba9b
RaheesKhan <rahees.khan92@gmail.com>
2017-11-21 16:17:14 +0530

    space modified

--- app/src/main/java/com/netcommlabs/greencontroller/sqlite_db/DatabaseHandler.java
+++ app/src/main/java/com/netcommlabs/greencontroller/sqlite_db/DatabaseHandler.java
@@ -20,7 +20,6 @@ import java.util.List;
 public class DatabaseHandler extends SQLiteOpenHelper {
 
     // All Static variables
-
     // Database Version
     private static final int DATABASE_VERSION = 1;
     // Database Name
